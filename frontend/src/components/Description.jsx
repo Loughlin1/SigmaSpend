3fdf12c2517fd96365b19b1bf29ca7776424aad1
Loughlin1 <loughlin.davidson@gmail.com>
2026-06-07 13:18:21 +0100

    Updating description

--- frontend/src/components/Description.jsx
+++ frontend/src/components/Description.jsx
@@ -3,10 +3,18 @@ import React, { useState, useEffect } from 'react';
 
 export default function DescriptionSection({}) {
   return (
-    <div>
+    <div style={{maxWidth: '600px'}}>
       <h3>Sigma Spend </h3>
-      <p>Sigma Spend is a tool for uploading and analysing expenses.</p>
+      <p style={{ fontSize: '1rem', lineHeight: '1.6', color: '#4a5568', margin: 0 }}>
+        Welcome to your smart financial workspace.
+      </p>
+      <br></br>
+      <p style={{ fontSize: '1rem', lineHeight: '1.6', color: '#4a5568', margin: 0 }}>
+       Securely upload bank statements, 
+        organise transactions across tailored subcategory hierarchies, and let our 
+        hybrid rule engine and local AI automatically classify your expense patterns 
+        with total privacy.
+      </p>
     </div>
-    
   );
 }
